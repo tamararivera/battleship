3140b865beaa353aa03407fa40420a8f7bb285e0
Tamara Rivera Farias <tamita.rivera@gmail.com>
2017-01-30 20:04:32 -0800

    Adding the loop and checking the user's guess.

--- battleship.js
+++ battleship.js
@@ -7,3 +7,13 @@ var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
+
+while (isSunk == false) {
+  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid cell number!");
+  }
+  else {
+    guesses++;
+  }
+}
